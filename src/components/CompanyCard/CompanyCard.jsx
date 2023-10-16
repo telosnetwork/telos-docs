7e01069a0bc4ec713edd7f51e82ff62698a107f7
Nathan Hennigh <nathanhennigh@gmail.com>
2023-10-16 16:10:55 -0500

    adding more dapps

--- src/components/CompanyCard/CompanyCard.jsx
+++ src/components/CompanyCard/CompanyCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const CompanyCard = ({ appName, logoURL, xURL, websiteURL, appURL, description }) => {
+const CompanyCard = ({ appName, logo, xURL, websiteURL, appURL, description }) => {
     
     return (
         <div className="app-card-container">
             <div className="app-card">
                 <div className="app-img-content">
-                    <img src={logoURL}/>
+                    <img src={logo}/>
                 </div>
                 <div className="app-content">
                     <p className="app-heading">{appName}</p>
@@ -16,9 +16,9 @@ const CompanyCard = ({ appName, logoURL, xURL, websiteURL, appURL, description }
                     </p>
                 </div>
             <div className="app-icons">
-                <a href={websiteURL}><img title="Website" className="card-icon" src={require('/static/img/website-icon-white.png').default} /></a>
-                <a href={appURL}><img title="Application"className="card-icon" src={require('/static/img/app-icon-white.png').default} /></a>
-                <a href={xURL}><img title="X (Twitter)" className="card-icon" src={require('/static/img/x-icon-white.png').default} /></a>
+                <a target="_blank" href={websiteURL}><img title="Website" className="card-icon" src={require('/static/img/website-icon-white.png').default} /></a>
+                <a target="_blank" href={appURL}><img title="Application"className="card-icon" src={require('/static/img/app-icon-white.png').default} /></a>
+                <a target="_blank" href={xURL}><img title="X (Twitter)" className="card-icon" src={require('/static/img/x-icon-white.png').default} /></a>
             </div>
         </div>
     </div>
