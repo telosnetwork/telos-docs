cc90dc5566fa499450d9c6bd13dcbd9076187f02
Nathan Hennigh <nathanhennigh@gmail.com>
2023-11-13 16:58:51 -0600

    created card for game info display

---
+++ src/components/GamingCard/GamingCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const GamingCard = ({ gameTitle, elixirLogo, chainLogo, descriptionHeading, descriptionText, videoUrl, website, nftPage, elixirPage }) => {
+    return (
+        <div className="gaming-main-card">
+            <div className='smaller-box'>
+                <div className="gaming-box-title">
+                    <h2>{gameTitle}</h2>
+                    <div className='links'>
+                        <a target="_blank" href={website}><img src={require('/static/img/logos/website-icon.png').default} alt="Website Icon" />Website</a>
+                        <a target="_blank" href={nftPage}><img src={require('/static/img/logos/altura.png').default} alt="NFT Page Icon" />NFT Mint Page</a>
+                        <a target="_blank" href={elixirPage}><img src={elixirLogo} alt="Elixir Page Icon" />Elixir Page</a>
+                    </div>
+                    <div className='logos'>
+                        <img src={chainLogo} />
+                    </div>
+                </div>
+                <div className="gaming-box-description">
+                    <h3>{descriptionHeading}</h3>
+                    <p>{descriptionText}</p>
+                </div>
+                <div className="gaming-box-video">
+                    <iframe src={videoUrl} title="Nope" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GamingCard;
