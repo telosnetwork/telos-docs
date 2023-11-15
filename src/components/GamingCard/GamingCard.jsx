7f1dae3d442bdd5c1f105cd94446c727a9b4b450
Nathan Hennigh <nathanhennigh@gmail.com>
2023-11-15 11:09:18 -0600

    updating gaming info

--- src/components/GamingCard/GamingCard.jsx
+++ src/components/GamingCard/GamingCard.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 
-const GamingCard = ({ gameTitle, elixirLogo, chainLogo, descriptionHeading, descriptionText, videoUrl, website, nftPage, elixirPage }) => {
+const GamingCard = ({ gameTitle, chain, descriptionText, mediaUrl, mediaType, website, nftPage, elixirPage }) => {
+    
+    const cardClass = chain === 'zero' ? 'gaming-main-card zero' : 'gaming-main-card';
+    
     return (
-        <div className="gaming-main-card">
-            <div className='smaller-box'>
+        <div className={cardClass}>
+            <div className='smaller-box'> 
                 <div className="gaming-box-title">
                     <h2>{gameTitle}</h2>
                     <div className='links'>
                         <a target="_blank" href={website}><img src={require('/static/img/logos/website-icon.png').default} alt="Website Icon" />Website</a>
                         <a target="_blank" href={nftPage}><img src={require('/static/img/logos/altura.png').default} alt="NFT Page Icon" />NFT Collections</a>
-                        <a target="_blank" href={elixirPage}><img src={elixirLogo} alt="Elixir Page Icon" />Elixir Page</a>
+                        <a target="_blank" href={elixirPage}><img src={require('/static/img/logos/elixir.png').default} alt="Elixir Page Icon" />Elixir Page</a>
                     </div>
                     <div className='logos'>
-                        <img src={chainLogo} />
+                        <img src={require('/static/img/logos/telos-' + chain + '.png').default} />
                     </div>
                 </div>
                 <div className="gaming-box-description">
-                    <h3>{descriptionHeading}</h3>
+                    <h3>About</h3>
                     <p>{descriptionText}</p>
                 </div>
                 <div className="gaming-box-video">
-                    <iframe src={videoUrl} title="Nope" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                    {mediaType === 'video' ? (
+                        <iframe src={mediaUrl} title="Trailer" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                    ) : (
+                        <img src={mediaUrl} alt="Game media" />
+                    )}
                 </div>
             </div>
         </div>
